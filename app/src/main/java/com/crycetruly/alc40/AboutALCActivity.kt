f6473253031e8b6b965161e0b353ec956a8e73f1
crycetruly <crycetruly@gmail.com>
2019-07-15 23:26:45 +0300

    ft(details):Create ALC Detail Views

---
+++ app/src/main/java/com/crycetruly/alc40/AboutALCActivity.kt
@@ -0,0 +1,12 @@
+package com.crycetruly.alc40
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutALCActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_alc)
+    }
+}
