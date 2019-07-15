5f5e3c3e42552cba049852d345081f7d937823db
crycetruly <crycetruly@gmail.com>
2019-07-15 23:27:39 +0300

    ft(intents):Implement multiscreen navigation

--- app/src/main/java/com/crycetruly/alc40/MainActivity.kt
+++ app/src/main/java/com/crycetruly/alc40/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.crycetruly.alc40
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.setTitle("Alc Phase1")
+        supportActionBar?.setTitle("ALC Phase1")
+
+        to_about.setOnClickListener { view-> startActivity( Intent(this,AboutALCActivity::class.java) )}
+        profilebtn.setOnClickListener { view->startActivity(Intent(this,ProfileActivity::class.java)) }
     }
 }
--- app/src/main/java/com/crycetruly/alc40/ProfileActivity.kt
+++ app/src/main/java/com/crycetruly/alc40/ProfileActivity.kt
@@ -2,11 +2,29 @@ package com.crycetruly.alc40
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        supportActionBar?.setTitle("My Profile")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+
+            android.R.id.home-> {
+                finish()
+                true
+
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+
     }
 }
